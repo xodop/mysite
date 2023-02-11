890ff009dcbd31ee557bdb9a7eb073b2d259528e
xodop <roachggw@gmail.com>
2023-02-11 03:59:26 +0300

    fix icon animation no load

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 "use strict"
+ 
+
+function readyLoad () {
+
+  const body = document.body;
+  body.classList.remove("preload");
+
+}
+
+
+function mouse_handler(event) {
+
+  if (event.type == 'mouseover') {
+    
+    event.target.classList.remove("icon-animation_mouseout")
+    event.target.classList.add("icon-animation_mouseover")
+  }
+  if (event.type == 'mouseout') {
+    event.target.classList.remove("icon-animation_mouseover")
+    event.target.classList.add("icon-animation_mouseout")
+  }
+
+}
+
+
+window.addEventListener("load", readyLoad);
+anime.onmouseover = anime.onmouseout = mouse_handler;
